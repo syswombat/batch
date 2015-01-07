fcb0c045a2c2d37a8b1b972faf3a306791d915b1
syswombat <github@kozo.ch>
2015-01-07 16:35:15 +0100

    Update linuxbasic.sh

--- linuxbasic.sh
+++ linuxbasic.sh
@@ -2,9 +2,14 @@
 cd /
 mkdir scripts
 cd /scripts
-apt-get update
-apt-get upgrade
-# VIM NOX Installation
-apt-get update
+apt-get update -y
+apt-get upgrade -y
+# VIM NOX Installation Set Line Number
+apt-get update -y
 apt-get install vim-nox -y
 
+
+# rootEmail send to Email Adress
+cd /
+cd /root
+echo yourEmail@yourDomain.ch >> /.forward

03e4598c4324dc9ffb823bfa8f5e3a3066d56788
Vincent Kocher <github@kozo.ch>
2015-01-07 18:52:58 +0100

    Update linuxbasic.sh

--- linuxbasic.sh
+++ linuxbasic.sh
@@ -11,6 +11,7 @@ wget -N https://raw.githubusercontent.com/syswombat/batch/master/addMuninnode.sh
 chmode u+x addMuninnode.sh
 clear
 apt-get update -y
+clear
 apt-get upgrade -y
 cd /
 echo "bye bye"

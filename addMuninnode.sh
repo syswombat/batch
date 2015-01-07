176fd424f38b9c9ea4cecd662e0ac2f61a9bcf5c
Vincent Kocher <github@kozo.ch>
2015-01-07 18:49:09 +0100

    Update addMuninnode.sh

--- addMuninnode.sh
+++ addMuninnode.sh
@@ -1,7 +1,11 @@
-#bash
+#!bin/bash
 # should only work on my system 
 # to create automatic a basic Munin-Node not a Master
 apt-get update -y
 apt-get upgrade -y
-apt-get install munin-node -y
+apt-get install -y munin-node munin-plugins-extra
+echo "allow ^10\.147\.42\.62$" >> /etc/munin/munin-node.conf
 
+/etc/init.d/munin-node restart
+Hostname -I
+echo "add the IP Adresse to the Munin-Master file"

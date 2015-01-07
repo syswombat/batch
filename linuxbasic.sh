a37806de38af6c3b57ca8db74da085b6587ba4a5
Vincent Kocher <github@kozo.ch>
2015-01-07 21:47:36 +0100

    Update linuxbasic.sh

--- linuxbasic.sh
+++ linuxbasic.sh
@@ -1,7 +1,9 @@
+################################################################################
 #/bin/bash
 # make a scriptsdir
 # if wget is not working do a ///  apt-get install ca-certificates
 # chmod +x linuxbasic.sh
+# run with ./linuxbasic.sh
 #
 #################################################################################
 

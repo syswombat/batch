eabfb4ad6614cb91ecf7a2cfc8a9c675170dd615
syswombat <github@kozo.ch>
2015-01-07 17:05:44 +0100

    Update linuxbasic.sh

--- linuxbasic.sh
+++ linuxbasic.sh
@@ -3,32 +3,7 @@
 cd /
 mkdir scripts
 cd /scripts
-wget 
+wget https://raw.githubusercontent.com/syswombat/batch/master/linuxbasic2.sh
 apt-get update -y
 apt-get upgrade -y
 cd /
-# VIM NOX Installation Set Line Number
-apt-get update -y
-apt-get install vim-nox -y
-
-
-# rootEmail send to Email Adress
-cd /
-cd /root
-echo yourEmail@yourDomain.ch >> /.forward
-
-
-# set Local Time CH Zürich
-cd /
-cd /ect
-rm localtime
-ln -s /usr/share/zoneinfo/europe/zurich localtime
-cd /
-
-# the end 
-Echo "Zeit / Datum Kontrollieren"
-Date
-Echo "Host Name"
-hostname
-
-

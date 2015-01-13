84c9a39dfcf539996beb8a5ec03db5a0386fa304
Vincent Kocher <github@kozo.ch>
2015-01-13 07:56:03 +0100

    Create inst_linux_dash.sh

---
+++ inst_linux_dash.sh
@@ -0,0 +1,9 @@
+################################################################################
+#/usr/bin/bash
+# make a scriptsdir
+# if wget is not working do a ///  apt-get install ca-certificates
+# chmod +x linuxbasic.sh
+# run with ./linuxbasic.sh
+#
+#################################################################################
+

74f991a19fd07716ba0439c743932a82f0c14b6d
Vincent Kocher <github@kozo.ch>
2015-01-13 07:59:36 +0100

    Update inst_linux_dash.sh

--- inst_linux_dash.sh
+++ inst_linux_dash.sh
@@ -1,9 +1,19 @@
 ################################################################################
 #/usr/bin/bash
-# make a scriptsdir
+# will Install Linux Dash on a Debian Server
 # if wget is not working do a ///  apt-get install ca-certificates
-# chmod +x linuxbasic.sh
-# run with ./linuxbasic.sh
+# chmod +x inst_linux_dash.sh
+# run with ./inst_linux_dash.sh
 #
 #################################################################################
 
+apt-get update -y
+apt-get upgrade -y
+apt-get install apache2 apache2-utils -y
+apt-get install php5 curl php5-curl php5-json -y
+apt-get install git -y
+
+service apache2 start
+
+cd /var/www
+git clone https://github.com/afaqurk/linux-dash.git

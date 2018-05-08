5c177d02fc82eadd224f094fe8f75bcc46524f79
Vincent Kocher <github@kozo.ch>
2018-05-08 05:52:19 +0000

    Update conkystartup.sh

--- conky/conkystartup.sh
+++ conky/conkystartup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # chmod a+x ~/conky-config/conkystartup.sh
 sleep 10
-conky -b -c ~/conky-config/conkyleftrc &
-conky -b -c ~/conky-config/conkyrightrc &
+conky -b -c /conky-config/conkyleftrc &
+conky -b -c /conky-config/conkyrightrc &

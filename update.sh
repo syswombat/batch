afa41c199473b2035c30d221c3630ceb47ffabb3
Vincent Kocher <github@kozo.ch>
2020-07-29 22:51:24 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -10,12 +10,12 @@ date +"%A %d. %B %Y | %T"
 ip a |grep inet
 # ip route get 1 | awk '{print $7;exit}'
 echo - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-systemctl --type=service | grep failed
+systemctl --type=service | grep failed --no-pager
 echo " = = = systemctl infos mit = = = "
 echo "systemctl --type=service | grep failed"
 echo "systemctl --type=service"
 echo - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # on a MediaVault installation 
-systemctl status openmediavault-engined.service
+systemctl status openmediavault-engined.service --no-pager
 echo ===========================================
-systemctl status openmediavault-issue.service
+systemctl status openmediavault-issue.service --no-pager
